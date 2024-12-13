df7eca0441d57b5b65512be5c7e9ea250761df74
ishisora <tatsu.gami2573@gmail.com>
2024-12-13 12:51:13 +0900

    usersオブジェクトで各セッションのroomを追えるようにする

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.post('/', function (req, res, next) {
     console.log(`mathingdebug1: ${keyword} `);
 
     let rooms = require('../service/rooms.js');
+    let users = require('../service/users.js');
     if (!(keyword in rooms)) { // keywordがroomsにない場合
         rooms[keyword] = {
             players: {
@@ -30,16 +31,18 @@ router.post('/', function (req, res, next) {
                 nowPlayer: 'o',
             }
         };
-        console.log(`roomsdebug1: ${JSON.stringify(rooms)}`);
+        users[req.session.userId] = keyword;
         res.redirect('/tictactoe');
     } else if (rooms[keyword].players.x === "" && rooms[keyword].players.o !== req.session.userId) { // keywordがroomsにあって、xがまだなくて、sessionがoと異なる場合
         rooms[keyword].players.x = req.session.userId;
+        users[req.session.userId] = keyword;
         console.log(`ZZZ matchingdebug2: ${rooms[keyword].players.x}`);
-        console.log(`roomsdebug1: ${JSON.stringify(rooms)}`);
         res.redirect('/tictactoe');
     } else {
         res.render('index.ejs', { title: 'tictactoe', session: req.session.userId, message: 'あいことばは既に使われています。他のあいことばを使用してください。' });
     }
+    console.log(`roomsdebug1: ${JSON.stringify(rooms)}`);
+    console.log(`usersdebug1: ${JSON.stringify(users)}`);
 });
 
 router.use('/tictactoe', require('./tictactoe.js'));
